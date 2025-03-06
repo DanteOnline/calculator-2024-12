acaa4d8766c4548fca306d62d71ffebb161596b9
Andre128 <131874342+Andre128@users.noreply.github.com>
2025-03-06 21:12:01 +0300

    Update division_without_remainde.py

--- calculator/division_without_remainde.py
+++ calculator/division_without_remainde.py
@@ -1,5 +1,6 @@
+"""My first Dosc in Github"""
 """division_without_remainder"""
 
 def division_without_remainder(a, b):
-    """function for summation"""
+    """function for division_without_remainder"""
     return a // b
